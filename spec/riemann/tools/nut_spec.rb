7a66e4e3beb8dc0dfd6d1f6f487f954aa62bf71f
Romain Tartière <romain@blogreen.org>
2022-10-17 13:40:05 -1000

    Add more unit tests

--- spec/riemann/tools/nut_spec.rb
+++ spec/riemann/tools/nut_spec.rb
@@ -21,6 +21,44 @@ RSpec.describe Riemann::Tools::Nut do
     end
   end
 
+  describe '#report_battery_voltage' do
+    it 'reports battery voltage' do
+      allow(subject).to receive(:report).with(
+        service: 'ups@localhost battery voltage',
+        metric: 13.4,
+        description: '13.4 V',
+        state: 'ok',
+      )
+      subject.report_battery_voltage
+      expect(subject).to have_received(:report)
+    end
+  end
+
+  describe '#report_input_voltage' do
+    it 'reports input voltage' do
+      allow(subject).to receive(:report).with(
+        service: 'ups@localhost input voltage',
+        metric: 230.0,
+        description: '230.0 V',
+        state: 'ok',
+      )
+      subject.report_input_voltage
+      expect(subject).to have_received(:report)
+    end
+  end
+
+  describe '#report_ups_alarm' do
+    it 'reports ups alarm' do
+      allow(subject).to receive(:report).with(
+        service: 'ups@localhost ups alarm',
+        description: 'Replace battery! No battery installed!',
+        state: 'critical',
+      )
+      subject.report_ups_alarm
+      expect(subject).to have_received(:report)
+    end
+  end
+
   describe '#report_ups_load' do
     it 'reports ups load' do
       allow(subject).to receive(:report).with(
@@ -30,6 +68,18 @@ RSpec.describe Riemann::Tools::Nut do
         state: 'ok',
       )
       subject.report_ups_load
+      expect(subject).to have_received(:report)
+    end
+  end
+
+  describe '#report_ups_status' do
+    it 'reports ups status' do
+      allow(subject).to receive(:report).with(
+        service: 'ups@localhost ups status',
+        description: 'ALARM OL RB',
+        state: 'critical',
+      )
+      subject.report_ups_status
       expect(subject).to have_received(:report)
     end
   end
